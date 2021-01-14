f90d03755b875460c2e02cc7f9afaace7990f36a
Mazharul Islam <72123654+cloud-007@users.noreply.github.com>
2021-01-14 20:26:20 +0600

    Update Ordered Set.cpp

--- Ordered Set.cpp
+++ Ordered Set.cpp
@@ -1,48 +1,9 @@
-/*One touch and i IGNITE*/
-#include<bits/stdc++.h>
-using namespace std;
-#define ll long long
-#define cloud_007 ios_base::sync_with_stdio(false);cin.tie(NULL)
-#define F first
-#define S second
-#define endl "\n"
-#define Endl "\n"
+#include<ext/pb_ds/tree_policy.hpp>
+#include<ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+ 
+typedef tree<pair < int, int >, null_type, less<pair < int, int>>, rb_tree_tag,
+       tree_order_statistics_node_update>
+       ordered_set;
 
-const int sz = 1e5 + 5;
-int a[sz], n, q, l, r, casee = 1, tc;
-ll tree[sz], x;
-
-void update(int idx, int val, int n) {
-    while(idx <= n) {
-        tree[idx] += val;
-        idx += idx & (-idx);
-    }
-}
-
-ll query(int idx) {
-    ll sum = 0;
-    while(idx > 0) {
-        sum += tree[idx];
-        idx -= idx & (-idx);
-    }
-    return sum;
-}//in main function while taking input update(i, value, range);
-
-
-int main() {
-#ifdef cloud007
-    freopen("in.txt", "r", stdin);
-#endif // cloud007
-    cloud_007;
-    int n;
-    cin >> n;
-    for(int i = 1; i <= n; i++) {
-        cin >> a[i];
-        update(1, a[i], n);
-    }
-
-    cout << query(n) - query(1) << endl;
-
-
-    return 0;
-}
+ordered_set os;
