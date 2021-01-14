d3f53f6773345f9e7d4af9dd092f6f85df99d0b5
Mazharul Islam <72123654+cloud-007@users.noreply.github.com>
2021-01-14 20:28:19 +0600

    Update Pair in unordered_map aka Custom Hash.cpp

--- Pair in unordered_map aka Custom Hash.cpp
+++ Pair in unordered_map aka Custom Hash.cpp
@@ -1,7 +1,7 @@
-#include<ext/pb_ds/tree_policy.hpp>
-#include<ext/pb_ds/assoc_container.hpp>
-using namespace __gnu_pbds;
+struct HASH {
+    size_t operator()(const pair<int, int>&x)const {
+        return hash<long long>()(((long long)x.first) ^ (((long long)x.second) << 32));
+    }
+};
 
-typedef tree<pair < int, int >, null_type, less<pair < int, int>>, rb_tree_tag,
-       tree_order_statistics_node_update>
-       ordered_set;
+unordered_map < pair < int, int >, ll, HASH > mp;
